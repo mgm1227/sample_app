089d01688ac12ad22bebfcb21f5433fbde506a6d
megumi <chory.chory.c12@gmail.com>
2022-10-10 06:10:07 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
+    assert_no_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
                                          password:              "password",
