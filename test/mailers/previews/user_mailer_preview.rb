e7e8190ba762b78e0f48e72b0320acc7d8c4a6f8
megumi <chory.chory.c12@gmail.com>
2022-10-17 13:33:14 +0000

    Add account activation

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,16 @@
+# Preview all emails at http://03c7d53d09814004ba7e791c46f483dd.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://03c7d53d09814004ba7e791c46f483dd.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer/account_activation
+  def account_activation
+    user = User.first
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+  end
+
+  # Preview this email at http://03c7d53d09814004ba7e791c46f483dd.vfs.cloud9.us-east-1.amazonaws.com/rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
+
+end
